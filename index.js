e36bc4a6a5367497769211a50ce93a5fc85479c3
martin <martin.rubiano1234@gmail.com>
2024-08-16 13:57:22 -0500

    Variables de enterno y carpeta Public

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
 const express = require('express')
+require('dotenv').config()
 
+console.log( process.env );
 
 
 // Crear el servidor de express
 const app = express();
 
-// Rutas
-app.get('/', (req, res) => {
+// Directorio Publico
+app.use( express.static('public'));
 
-    res.json({
-        ok: true
-    })
+
+// Rutas
+// app.get('/', (req, res) => {
+
+//     res.json({
+//         ok: true
+//     })
     
 
-});
+// });
 
 
 
 // Escuchar peticiones
-app.listen( 4000, () => {
-    console.log('Servidor corriendo en puerto 4000');
+app.listen( process.env.PORT, () => {
+    console.log(`Servidor corriendo en puerto ${ process.env.PORT }`);
     
 } )
