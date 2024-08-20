edc5c8377a134584f3d4f8d48d1460be0a3960ba
martin <martin.rubiano1234@gmail.com>
2024-08-20 13:13:40 -0500

    Express Validator

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,26 @@
 const { Router } = require('express')
 const router = Router()
 const { crearUsuario, loginUsuario, revalidarUsuario } = require('../controllers/auth')
+const { check } = require('express-validator')
 
-router.post('/new', crearUsuario );
+router.post(
+    '/new', 
+    [ // MiddleWare
+        check('name', 'Nombre es obligatorio').not().isEmpty(),
+        check('email', 'El Email es obligatorio').isEmail(),
+        check('password', 'Password debe de ser de 6 caracteres').isLength({ min: 6 })
+     ] , 
+    crearUsuario 
+);
 
-router.post('/', loginUsuario);
+router.post(
+    '/', 
+    [ // MiddleWare
+        check('email', 'El Email es obligatorio').isEmail(),
+        check('password', 'Password debe de ser de 6 caracteres').isLength({ min: 6 })
+    ],
+    loginUsuario
+);
 
 router.get('/renew', revalidarUsuario);
 
