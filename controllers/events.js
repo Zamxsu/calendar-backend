2d7d3cf66cc611b39594548f2cdb6daf62eab0ea
martin <martin.rubiano1234@gmail.com>
2024-08-21 13:30:05 -0500

    editando crudx

--- controllers/events.js
+++ controllers/events.js
@@ -1,63 +1,41 @@
 const { response } = require('express')
-const { generarJWT } = require('../helpers/jwt')
-
 
 const getEventos = async( req, res = response ) => {
-
-    const uid = req.uid;
-    const name = req.name
-
-    const token = await generarJWT( uid, name )
 
     res.json({
         ok: true,
         msg: 'getEventos',
-        token
+
     })
 
 }
 
 const crearEvento = async( req, res = response ) => {
 
-    const uid = req.uid;
-    const name = req.name
-
-    const token = await generarJWT( uid, name )
-
     res.json({
         ok: true,
         msg: 'crearEvento',
-        token
+        
     })
 
 }
 
 const actualizarEvento = async( req, res = response ) => {
 
-    const uid = req.uid;
-    const name = req.name
-
-    const token = await generarJWT( uid, name )
-
     res.json({
         ok: true,
         msg: 'actualizarEvento',
-        token
+        
     })
 
 }
 
 const eliminarEvento = async( req, res = response ) => {
 
-    const uid = req.uid;
-    const name = req.name
-
-    const token = await generarJWT( uid, name )
-
     res.json({
         ok: true,
         msg: 'eliminarEvento',
-        token
+        
     })
 
 }
