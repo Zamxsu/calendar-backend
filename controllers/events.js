c98e62a512c95f658d5ec1d194f21e4d185d5841
martin <martin.rubiano1234@gmail.com>
2024-08-22 15:37:06 -0500

    Acttializar evento

--- controllers/events.js
+++ controllers/events.js
@@ -43,12 +43,51 @@ const crearEvento = async( req, res = response ) => {
 
 const actualizarEvento = async( req, res = response ) => {
 
-    res.json({
-        ok: true,
-        msg: 'actualizarEvento',
-        
-    })
+    const eventoId = req.params.id;
 
+    try {
+        
+        const evento = await Evento.findById( eventoId )
+        const uid = req.uid
+
+        if( !evento ) {
+
+            res.status(404).json({
+                ok: false,
+                msg: 'Evento no existe por ese ID'
+            })
+        }
+
+        if( evento.user.toString() !== uid ) {
+
+            return res.status( 401 ).json({
+                ok: false,
+                msg: 'No tiene privilegio de editar ese evento'
+            })
+                
+        }
+
+        const nuevoEvento = {
+            ...req.body,
+            user: uid
+        }
+
+        const eventoActualizado = await Evento.findByIdAndUpdate( eventoId, nuevoEvento, { new: true } )
+
+        res.json({
+            ok: true,
+            evento: eventoActualizado
+        })
+
+    } catch (error) {
+        
+        console.log( error );
+        res.status(500).json({
+            ok: false,
+            msg: 'Hable con el administrador'
+        })
+
+    }
 }
 
 const eliminarEvento = async( req, res = response ) => {
